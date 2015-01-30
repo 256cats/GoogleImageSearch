32a3c7412fad8e8792d2f0767ed9bce08904552a
Andrey E <andrey@256cats.com>
2015-01-30 03:21:40 -0500

    Add search by local image functionality

--- GoogleImageSearch.php
+++ GoogleImageSearch.php
@@ -2,7 +2,8 @@
 include_once __DIR__.'/simple_html_dom.php';
 define('COOKIE_FILE', __DIR__.'/cookie.txt');
 @unlink(COOKIE_FILE); //clear cookies before we start
-
+define('CURL_LOG_FILE', __DIR__.'/request.txt');
+@unlink(CURL_LOG_FILE);//clear curl log
 class GoogleImageSearch {
 
     private $searchUrl = 'https://www.google.com/searchbyimage?image_url=';
@@ -13,21 +14,40 @@ class GoogleImageSearch {
      * @param $url
      * @return bool|simple_html_dom
      */
-    public function getDom($url) {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
+    public function getDom($url, $post = false) {
+        $f = fopen(CURL_LOG_FILE, 'a+'); // curl session log file
+        $curlOptions = array(
+            CURLOPT_ENCODING => 'gzip,deflate',
+            CURLOPT_AUTOREFERER => 1,
+            CURLOPT_CONNECTTIMEOUT => 120, // timeout on connect
+            CURLOPT_TIMEOUT => 120, // timeout on response
             CURLOPT_URL => $url,
             CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_MAXREDIRS => 9,
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_HEADER => 0,
             CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
             CURLOPT_COOKIEFILE => COOKIE_FILE,
-            CURLOPT_COOKIEJAR => COOKIE_FILE
-        ));
-        $dom = str_get_html(curl_exec($curl));
+            CURLOPT_COOKIEJAR => COOKIE_FILE,
+            CURLOPT_STDERR => $f, // log session
+            CURLOPT_VERBOSE => true,
+            CURLINFO_HEADER_OUT  => true,
+        );
+        $curl = curl_init();
+        curl_setopt_array($curl, $curlOptions);
+
+        if($post) { // add post options
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+        }
+        $data = curl_exec($curl);
         curl_close($curl);
+        $dom = str_get_html($data);
+        fwrite($f, "-----------------------------------------------------------\n\n");
+        fclose($f);
+
         return $dom;
     }
 
@@ -36,7 +56,8 @@ class GoogleImageSearch {
      * @return bool|simple_html_dom
      * @throws Exception
      */
-    public function getImageSearchDom($url) {
+    public function getImageUrlSearchDom($url) {
+        //echo $url;
         $dom = $this->getDom($url);
         if(stripos($dom->find('title', 0), '302 Moved') !== false) { // if '302 moved' page, follow link
             $a = $dom->find('a', 0)->href;
@@ -74,12 +95,33 @@ class GoogleImageSearch {
         //links, so skip the first page
 
         $d = $dom->find('div.srg', $c); // get second div(if this is 1st page), or first div
+
         foreach($d->find('div.rc h3.r') as $h3) {
             foreach($h3->find('a') as $a) { // get links
                 $result[] = array(htmlspecialchars_decode($a->plaintext, ENT_QUOTES), $a->href);
             }
         }
         return $result;
+    }
+
+    /**Upload local image to Google and get result page
+     * @param $fileName
+     * @return bool|simple_html_dom
+     */
+    public function getLocalImageSearchDom($fileName) {
+        //based on http://stackoverflow.com/a/18936880
+        list($w, $h) = getimagesize($fileName);
+        $dom = $this->getDom('https://www.google.com/searchbyimage/upload', array(
+                'encoded_image' => '@'.realpath($fileName),
+                'image_url' => '',
+                'image_content' => '',
+                'filename' => '',
+                'h1' => 'en',
+                'bih' => $h,
+                'biw' => $w
+            )
+        );
+        return $dom;
     }
 
 
@@ -98,13 +140,13 @@ class GoogleImageSearch {
      */
     public function search($imageUrl, $numPages = 1) {
         try {
-            $dom = $this->getImageSearchDom($this->searchUrl.$imageUrl); // get first page dom
+            $dom = is_file($imageUrl) ? $this->getLocalImageSearchDom($imageUrl) : $this->getImageUrlSearchDom($this->searchUrl.$imageUrl); // get first page dom
             $bestGuess = $this->getBestGuess($dom); // get best guess from 1st page
             $searchResults = $this->getSearchResults($dom); // get search results from 1st page
             $nextPageA = $dom->find('#nav a.pn', 0); // check if we have 'next page' link (if we don't - it's the only page)
             $dom->clear();
             for($i = 1; $i < $numPages && $nextPageA; $i++) { // loop through pages [2 - $numPages]
-                $dom = $this->getImageSearchDom($this->searchUrl.$imageUrl.'&start='.($i * 10));
+                $dom = $this->getImageUrlSearchDom($this->googleDomain.htmlspecialchars_decode($nextPageA->href));
                 $searchResults = array_merge($searchResults, $this->getSearchResults($dom));// get search results from page and merge with available results
                 $nextPageA = $dom->find('#nav a.pn', 0); // check if we have 'next page' link (if we don't - it's last page)
                 $dom->clear();
--- example.php
+++ example.php
@@ -2,7 +2,22 @@
 include_once __DIR__.'/GoogleImageSearch.php';
 
 $imageSearch = new GoogleImageSearch();
+echo "Search by image URL: <br />\n";
 if($results = $imageSearch->search('http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg', 2)) {
+    if($results['search_results']) {
+        echo "Best guess: <strong><a href=\"{$results['best_guess'][1]}\">{$results['best_guess'][0]}</strong><br />\n";
+        echo "<ol><br />\n";
+        foreach($results['search_results'] as $k => $r) {
+            echo "<li><a href=\"{$r[1]}\">{$r[0]}</a></li>\n";
+        }
+        echo "</ol><br />\n";
+    } else {
+        echo 'Nothing found';
+    }
+
+}
+echo "Search by uploading local image: <br />\n";
+if($results = $imageSearch->search('test.jpg', 2)) {
     if($results['search_results']) {
         echo "Best guess: <strong><a href=\"{$results['best_guess'][1]}\">{$results['best_guess'][0]}</strong><br />\n";
         echo "<ol><br />\n";
